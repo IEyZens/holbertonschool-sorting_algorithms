fd610d67c9581232dce9984e02a2b1b3f94b4c2e
IEyZens <roncint61@gmail.com>
2025-04-10 08:53:28 +0200

    Add task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ *
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp, swap;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		swap = 0;
+
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+				swap = 1;
+			}
+		}
+
+		if (swap == 0)
+			break;
+	}
+}
