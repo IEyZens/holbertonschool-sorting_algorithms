c5a0fd5b3003b7c388677b4f8033480d68a6317f
IEyZens <roncint61@gmail.com>
2025-04-10 18:27:34 +0200

    Fix code bug

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,43 +6,33 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	if (!list || !(*list) || !(*list)->next)
+	listint_t *current, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	listint_t *current = (*list)->next;
-
+	current = (*list)->next;
 	while (current != NULL)
 	{
-		listint_t *target = current;
-		listint_t *prev = current->prev;
-
-		while (prev != NULL && prev->n > target->n)
-			prev = prev->prev;
-
-		if (prev != current->prev)
+		temp = current;
+		while (temp->prev != NULL && temp->n < temp->prev->n)
 		{
-			if (current->next)
-				current->next->prev = current->prev;
-			if (current->prev)
-				current->prev->next = current->next;
 
-			if (prev == NULL)
-			{
-				target->next = *list;
-				target->prev = NULL;
-				(*list)->prev = target;
-				*list = target;
-			}
+			temp->prev->next = temp->next;
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
+
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
+
+			if (temp->prev != NULL)
+				temp->prev->next = temp;
 			else
-			{
-				target->next = prev->next;
-				if (prev->next)
-					prev->next->prev = target;
-				prev->next = target;
-				target->prev = prev;
-			}
-		}
+				*list = temp;
 
+			print_list(*list);
+		}
 		current = current->next;
 	}
 }
